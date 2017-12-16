f804987f8466741d36afaaf11a806d787e2d474e
Matthew Stockinger <monkeygrp@gmail.com>
2017-12-15 21:14:13 -0600

    increased to 6 images.

--- js/codeArt.js
+++ js/codeArt.js
@@ -22,7 +22,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("navdots");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
