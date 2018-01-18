072a259aaf6d583c88e677e049f60ceee29209f9
Matthew Stockinger <monkeygrp@gmail.com>
2018-01-18 16:00:54 -0600

    changed active dot color to black

--- js/codeArt.js
+++ js/codeArt.js
@@ -29,9 +29,9 @@ function showDivs(n) {
         x[i].style.display = "none";  
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-blue", "");
+        dots[i].className = dots[i].className.replace(" w3-black", "");
     }
     x[slideIndex-1].style.display = "block";  
     // dots[slideIndex-1].className += " w3-white";
-    dots[slideIndex - 1].className += " w3-blue";
+    dots[slideIndex - 1].className += " w3-black";
 }
