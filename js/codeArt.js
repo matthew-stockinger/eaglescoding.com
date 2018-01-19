364c5dfcf8d56ab5256dd2f9fa31229809304593
Matthew Stockinger <monkeygrp@gmail.com>
2018-01-18 21:03:21 -0600

    Moved imageNav generation to JS.  Added logic to generate a number-based nav for 8 or more images.

--- js/codeArt.js
+++ js/codeArt.js
@@ -9,6 +9,15 @@ function w3_close() {
 
 // Slideshow
 var slideIndex = 1;
+var numFigures = document.getElementsByTagName("figure").length;
+if (numFigures === 1) {
+    var navType = "noNav";
+} else if (numFigures > 1 && numFigures <= 7) {
+    var navType = "dotNav";
+} else if (numFigures >= 8) {
+    var navType = "numNav";
+}
+displayNav(navType);
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -34,4 +43,56 @@ function showDivs(n) {
     x[slideIndex-1].style.display = "block";  
     // dots[slideIndex-1].className += " w3-white";
     dots[slideIndex - 1].className += " w3-black";
+}
+
+function displayNav(navType) {
+    switch(navType) {
+        case "noNav":
+            break;
+        case "dotNav":
+            displayDotNav();
+            break;
+        case "numNav":
+            displayNumNav();
+            break;
+    }
+}
+
+function displayDotNav() {
+    var leftArrow = '<span class="w3-hover-black w3-round unselectable navarrow" onclick="plusDivs(-1)">&#10094;</span>';
+    var rightArrow = '<span class="w3-hover-black w3-round unselectable navarrow" onclick="plusDivs(1)">&#10095;</span>';
+    var navDotA = '<span class="w3-badge navdots w3-border w3-border-black w3-transparent w3-hover-black" onclick="currentDiv(';
+    var navDotB = ')"></span>';
+    var navDots = '';
+    var i;
+    for (i = 1; i <= numFigures; i++) {
+        navDots += navDotA + i + navDotB;
+    }
+    document.getElementById("imageNav").innerHTML = leftArrow + navDots + rightArrow;
+}
+
+function displayNumNav() {
+    var leftArrow = '<span class="w3-hover-black w3-round unselectable navarrow" onclick="plusDivs(-1)">&#10094;</span>';
+    var rightArrow = '<span class="w3-hover-black w3-round unselectable navarrow" onclick="plusDivs(1)">&#10095;</span>';
+    var navNumberA = '<span class="navnums w3-hover-text-blue" onclick="currentDiv(';
+    var navNumberB = ')">'
+    var navNumberC = '</span>';
+    var navNumbers = "";
+    if (slideIndex <= 4) {
+        var i;
+        for (i = 1; i <= 7; i++) {
+            navNumbers += navNumberA + i + navNumberB + i + navNumberC;
+        }
+    } else if (slideIndex > 4 && slideIndex <= numFigures - 3) {
+        var i;
+        for (i = slideIndex - 3; i <= slideIndex + 3; i++) {
+            navNumbers += navNumberA + i + navNumberB + i + navNumberC;
+        }
+    } else if (slideIndex > numFigures - 3) {
+        var i;
+        for (i = numFigures - 6; i <= numFigures; i++) {
+            navNumbers += navNumberA + i + navNumberB + i + navNumberC;
+        }
+    }
+    document.getElementById("imageNav").innerHTML = leftArrow + navNumbers + rightArrow;
 }
