8e8c08ea4e6464bad8684b231b5b87dc9552cea3
monkeygrp <monkeygrp@gmail.com>
2018-02-15 09:06:07 -0600

    updated main nav in index.html

--- js/index.js
+++ js/index.js
@@ -1,29 +1,48 @@
-function w3_open() {
-    document.getElementById("mySidenav").style.display = "block";
+/******************  Hamburger/side menu ********************* */
+function openMainNav() {
+    var mySideNav = document.getElementById("mySideNav");
+    mySideNav.style.display = "flex";
+    if(mySideNav.className.indexOf(" w3-animate-left") === -1) {
+        mySideNav.className += " w3-animate-left";
+    }
 }
 
-function w3_close() {
-    document.getElementById("mySidenav").style.display = "none";
+function closeMainNav() {
+    document.getElementById("mySideNav").style.display = "none";
 }
 
-function toggleArt() {
-    var topImg = document.getElementById("topImg");
-    var topArt = document.getElementById("topArt");
-    // toggle landing page image display.
-    if (topImg.className.indexOf("w3-show") > -1) {
-        topImg.className = topImg.className.replace("w3-show", "w3-hide");
-    } else if (topImg.className.indexOf("w3-hide") > -1) {
-        topImg.className = topImg.className.replace("w3-hide", "w3-show");
-    } else {
-        topImg.className += " w3-hide";
+function openCodeArtNav() {
+    var mySideNav = document.getElementById("mySideNav");
+    mySideNav.className = mySideNav.className.replace(" w3-collapse",""); 
+    mySideNav.style.display = "none";
+    var codeArtNav = document.getElementById("codeArtNav");
+    codeArtNav.style.display = "flex";
+    // need this if to get animation to run the first time user opens the submenu.
+    // otherwise it'll just appear with no animation.
+    if (codeArtNav.className.indexOf(" w3-animate-left") === -1) {
+        codeArtNav.className += " w3-animate-left";
     }
+}
 
-    // toggle art carousel display.
-    if (topArt.className.indexOf("w3-show") > -1) {
-        topArt.className = topArt.className.replace("w3-show", "w3-hide");
-    } else if (topArt.className.indexOf("w3-hide") > -1) {
-        topArt.className = topArt.className.replace("w3-hide", "w3-show");
-    } else {
-        topArt.className += " w3-hide";
+function closeCodeArtNav() {
+    document.getElementById("codeArtNav").style.display = "none";
+}
+
+function backToMainNav() {
+    document.getElementById("codeArtNav").style.display = "none";
+    var mySideNav = document.getElementById("mySideNav");
+    mySideNav.className += " w3-collapse";
+    if(mySideNav.className.indexOf(" w3-animate-left") === -1) {
+        mySideNav.className += " w3-animate-left";
     }
+    mySideNav.style.display = "flex";
+}
+
+function highlightMe(elt) {
+    var codeArtNavDivs = document.getElementsByClassName("codeArtNavDivs");
+    var i;
+    for (i = 0; i < codeArtNavDivs.length; i++) {
+        codeArtNavDivs[i].className = codeArtNavDivs[i].className.replace(" w3-gray", " w3-black");
+    }
+    elt.className = elt.className.replace(" w3-black", " w3-gray");
 }
